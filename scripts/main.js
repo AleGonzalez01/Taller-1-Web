aeef4c91a3d57ed42d0d5c33778d614935d254c3
Alejandra <maljagonzalez01@gmail.com>
2020-09-22 17:39:01 -0500

    js de la parte de interacción

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,23 @@
 const btns = document.querySelectorAll('.interaction__buttons .buttons');
 const image = document.querySelector('interaction__image');
-let numberimages= 0;
-console.log(numberimages);
 
 btns.forEach(function(btn){
 
     btn.addEventListener('click', function(){
         btn.classList.toggle("buttons--blacks");
-       
+        let name = "";
+        btns.forEach(function(b){
+            name += b.classList.contains("buttons--blacks");
+        })
+        console.log(name);
+        image.style.backgroundImage = 'url(./imagenes/'+name+'.png)';
     })
 })
 
-btns[0].addEventListener('click', function(){
-    numberimages=1;
-    console.log(numberimages);
-})
 
-btns[1].addEventListener('click', function(){
-    numberimages=2;
-    console.log(numberimages);
-})
 
-btns[2].addEventListener('click', function(){
-    numberimages=3;
-    console.log(numberimages);
-})
 
-if(numberimages==1){
-    image.style.backgroundImage = "url('+ /imagenes/Soloconsola +')";
-}
+
 
 
 
